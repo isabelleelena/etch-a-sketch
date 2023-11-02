6ac9306e9335f0f10db885f98d6f5d2df2b7bb1b
Isabelle Joubert <isabelleelenajoubert@gmail.com>
2023-11-02 10:55:07 +0200

    Add class to grid divs

--- scripts.js
+++ scripts.js
@@ -3,5 +3,6 @@ container.setAttribute('id', 'container');
 
 for (i = 0; i < 16; i++) {
     const div = document.createElement('div');
+    div.classList.add('grid-square');
     container.appendChild(div);
 }
