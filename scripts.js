10d0915b6914278e8f851eda56945443729a2e1f
Isabelle Joubert <isabelleelenajoubert@gmail.com>
2023-11-02 11:07:19 +0200

    Add styles and fix stylesheet link error.

--- scripts.js
+++ scripts.js
@@ -1,6 +1,3 @@
-const container = document.createElement('div');
-container.setAttribute('id', 'container');
-
 for (i = 0; i < 16; i++) {
     const div = document.createElement('div');
     div.classList.add('grid-square');
