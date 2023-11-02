6e04062d048c7bbcfe84f857d9eeb8563ae9af74
Isabelle Joubert <isabelleelenajoubert@gmail.com>
2023-11-02 10:52:38 +0200

    Add 16 divs to html using js.

--- scripts.js
+++ scripts.js
@@ -1,3 +1,7 @@
 const container = document.createElement('div');
 container.setAttribute('id', 'container');
 
+for (i = 0; i < 16; i++) {
+    const div = document.createElement('div');
+    container.appendChild(div);
+}
